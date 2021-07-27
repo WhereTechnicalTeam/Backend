3a06cc51a0629b17d1654c2686754afe153fed1f
jed <jmadjanor6@gmail.com>
2021-07-27 18:19:28 +0000

    commented test.py

--- app_contacts/tests.py
+++ app_contacts/tests.py
@@ -1,17 +1,17 @@
-from django.test import TestCase, Client
+# from django.test import TestCase, Client
 
-# Create your tests here.
-class UsersViewTestCase(TestCase):
+# # Create your tests here.
+# class UsersViewTestCase(TestCase):
 
-    def setUp(self):
+#     def setUp(self):
 
-        #Create the client object
-        self.client = Client()
+#         #Create the client object
+#         self.client = Client()
 
 
-    def test_contactpage_is_accessible(self):
-        """
-        The contact page must not be accessible without authentication          
-        """
-        response = self.client.get("contacts/index/")
-        self.assertEqual(response.status_code, 404)
+#     def test_contactpage_is_accessible(self):
+#         """
+#         The contact page must not be accessible without authentication          
+#         """
+#         response = self.client.get("contacts/index/")
+#         self.assertEqual(response.status_code, 404)
