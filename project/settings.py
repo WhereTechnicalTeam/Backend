4cb68d92227fd81511da756db94f87139d253b09
INK <isaacnewtonfx@gmail.com>
2021-08-13 13:55:47 +0000

    secured email credentials

--- project/settings.py
+++ project/settings.py
@@ -176,8 +176,8 @@ SITE_ID=1
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'wheregeospatialnoreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'WhereIsHere'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
 
